7388273ade76ca6c679e48528fbc712b8e20ffad
LAB321 Student12 <lab321Student12@gau.local>
2025-04-07 11:47:54 +0400

    lect9

---
+++ Lecture-9/Lecture-9/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lecture_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FilterBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(283, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 280);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // FilterBtn
+            // 
+            this.FilterBtn.Location = new System.Drawing.Point(283, 336);
+            this.FilterBtn.Name = "FilterBtn";
+            this.FilterBtn.Size = new System.Drawing.Size(138, 43);
+            this.FilterBtn.TabIndex = 1;
+            this.FilterBtn.Text = "Filter Quantity > 10";
+            this.FilterBtn.UseVisualStyleBackColor = true;
+            this.FilterBtn.Click += new System.EventHandler(this.FilterBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(202, 50);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 2;
+            this.resetBtn.Text = "Reset FIlter";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.FilterBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button FilterBtn;
+        private System.Windows.Forms.Button resetBtn;
+    }
+}
+
---
+++ Lecture-9/Lecture-9/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VynilStoreEntities db = new VynilStoreEntities();
+            var album = db.Albums.ToList();
+            dataGridView1.DataSource = album;
+        }
+
+        private void FilterBtn_Click(object sender, EventArgs e)
+        {
+            VynilStoreEntities db = new VynilStoreEntities();
+            var album2 = db.Albums.ToList();
+            var filteredAlbums = db.Albums.Where(x => x.inStock > 10).ToList();
+            dataGridView1.DataSource = filteredAlbums;
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            VynilStoreEntities db = new VynilStoreEntities();
+            var album = db.Albums.ToList();
+            dataGridView1.DataSource = album;
+        }
+    }
+}
